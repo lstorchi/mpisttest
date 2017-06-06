866abfcf093fdc4f794c092f398fdd9acaf803d5
Loriano Storchi <loriano@storchi.org>
2017-06-06 11:12:37 +0200

    minor2

--- helloworld/hello_mpi.c
+++ helloworld/hello_mpi.c
@@ -14,19 +14,19 @@ int main (int argc , char * argv [])
   /* 2nd: request a thread id , sometimes called a " rank " from
    * the MPI master process , which has rank or tid == 0 */
   
-  MPI_Comm_rank ( MPI_COMM_WORLD , & tid );
+  MPI_Comm_rank ( MPI_COMM_WORLD , &tid );
   /* 3rd: this is often useful , get the number of threads
    * or processes launched by MPI , this should be NCPUs -1
    * */
 
-  MPI_Comm_size ( MPI_COMM_WORLD , & nthreads );
+  MPI_Comm_size ( MPI_COMM_WORLD , &nthreads );
   /* on EVERY process , allocate space for the machine name */
   cpu_name = ( char *) calloc (80 , sizeof ( char ));
   
   /* get the machine name of this particular host ... well
    * at least the first 80 characters of it ... */
   gethostname ( cpu_name ,80) ;
-  printf (" hello MPI user : from process = %i on machine =%s, of NCPU =%i processes \n",
+  printf (" hello MPI user : from process = %i on machine = %s, of NCPU = %i processes \n",
       tid , cpu_name , nthreads );
 
   MPI_Finalize ();
